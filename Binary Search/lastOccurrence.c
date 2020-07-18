724054929a9eaaae6b36e6c308dbf3514492f425
Vivesh Yadav <viveshofficial@gmail.com>
2020-07-18 15:26:07 +0545

    Add occurrence

---
+++ Binary Search/lastOccurrence.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int binarySearch(int array[], int n, int key){
+  int low, high, mid, result;
+  
+  low=0;
+  high = n-1;
+  result = -1;
+  
+  while(low <=high){
+        mid = (low + high) /2;
+        
+      if(key == array[mid]){
+          low = mid + 1;
+          result = mid;
+      } else if (key > array[mid]){
+          low = mid + 1;
+      }else{
+          high = mid -1 ;
+      }
+  }
+  return result;
+}
+int main(){
+    int size,key;
+    
+    printf("Enter size of array .\n");
+    scanf("%d",&size);
+    
+    int array[size];
+    
+    printf("Enter the elements.\n");
+    int i;
+    for(i=0;i<size;i++){
+        scanf("%d",&array[i]);
+    }
+    
+    printf("Key to find :\n");
+    scanf("%d", &key);
+    int result = binarySearch(array, size, key);
+    if(result !=-1){
+        printf("Last occurrence at %d index\n", result);
+    }else{
+        printf("Key not found.\n");
+    }
+    
+}
